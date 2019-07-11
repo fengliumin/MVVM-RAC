b7df6e48f384ef5bd54bdbf26a31854285b10ac2
fengliumin <fengliumin@163.com>
2019-07-11 11:56:16 +0800

    Initial Commit

---
+++ MVVM+RAC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MVVM+RAC
+//
+//  Created by abc on 2019/7/11.
+//  Copyright © 2019 abc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MVVM+RACTests/MVVM_RACTests.m
@@ -0,0 +1,37 @@
+//
+//  MVVM_RACTests.m
+//  MVVM+RACTests
+//
+//  Created by abc on 2019/7/11.
+//  Copyright © 2019 abc. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MVVM_RACTests : XCTestCase
+
+@end
+
+@implementation MVVM_RACTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
